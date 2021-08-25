62ebe3541609ddfaf79a6a600cf0b4c4f559a4bb
iyesogie <79468835+iyesogie@users.noreply.github.com>
2021-08-24 21:57:44 -0400

    Nav Update

--- Project 3 Group 5-2/Final_project/app.py
+++ Project 3 Group 5-2/Final_project/app.py
@@ -13,9 +13,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+#@app.route("/about")
+#def about():
+    #return render_template('about.html')#
 
 
 @app.route("/viz1")
